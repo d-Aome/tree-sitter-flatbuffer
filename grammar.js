f184660c3a1593ba4b2e97bbe17f4f005cb05096
David Montiel <davidmontiel549@gmail.com>
2026-04-05 16:14:48 -0700

    Update identifer regex and add regex for integers
    Identifer regex update:
    - Allow identifier to include `_`.
    Integers Regex:
    - Add regex for decimal Integer digits
    - Add regex for Hexidecimal Integer digits

--- grammar.js
+++ grammar.js
@@ -1,5 +1,9 @@
+// to stop eslint from complaining
+
+/* global grammar, repeat, field, choice, seq, reqeat1, optional, prec, .left, .right, .dynamic*/
+
 /**
- * @file A parser for google flatbuffers
+ * @file A parser for Google flatbuffers
  * @author David Montiel <davidm0ntiel@proton.me>
  * @license MIT
  */
@@ -7,22 +11,44 @@
 /// <reference types="tree-sitter-cli/dsl" />
 // @ts-check
 
+/** @description Regex pattern for hexidecimal digits */
+let h_digit = '0-9a-fA-F';
+
 export default grammar({
   name: 'flatbuffer',
 
   rules: {
-    // TODO: add the actual grammar rules
     source_file: ($) => repeat($._declaration),
 
     _declaration: ($) =>
       choice($.namespace_declaration, $.attribute_declaration),
 
+    // EBNF: 'namespace' ident ( `.` ident )* `;`
     namespace_declaration: ($) =>
-      seq('namespace', field('name', $.identifier), ';'),
+      seq(
+        'namespace',
+        field(
+          'ident',
+          seq($.identifier, repeat(optional(seq('.', $.identifier)))),
+        ),
+        ';',
+      ),
 
     attribute_declaration: ($) =>
-      seq('attribute', field('name', $.identifier), ';'),
+      seq(
+        'attribute',
+        field('name', choice($.identifier, seq('"', $.identifier, '"'))),
+        ';',
+      ),
+    identifer: ($) => /[a-zA-Z_][a-zA-Z0-9_]*/,
+    /* digits */
 
-    identifier: ($) => /[a-zA-Z][a-zA-Z0-9]*/,
+    /* Integers */
+    decimal_integer_constant: () => /[-+]?[\d]+/,
+    hex_integer_constant: () => new RegExp(`[-+]?0[xX][${h_digit}]+`),
+
+    // EBND:dec_integer_constant | hex_integer_constant
+    integer_constant: ($) =>
+      choice($.decimal_integer_constant, $.hex_integer_constant),
   },
 });
